06e5c6a9f6a57a3af10c46252529a2d210550b08
NicolasCrapanzano <nicolaslcrapanzano@gmail.com>
2022-09-13 12:19:30 -0300

    added calls to redis

--- build/index.js
+++ build/index.js
@@ -1,15 +1,92 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
-console.log("hello world");
+const redis = __importStar(require("redis"));
+const express_1 = __importDefault(require("express"));
+const client = redis.createClient();
+const app = (0, express_1.default)();
+client.on('error', (err) => console.log('Redis Client Error', err));
+connect();
 const fileName = "./src/sample.txt";
-fs_1.default.readFile(fileName, "utf8", (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log("data: ", data);
-});
+const key = "text";
+main();
+function main() {
+    setToDB(key, readfile(fileName));
+    setTimeout(() => {
+        getDataFromDB();
+    }, 5000);
+    setTimeout(() => {
+        deleteDataFromDB(key);
+    }, 7000);
+    setTimeout(() => {
+        getDataFromDB();
+    }, 8000);
+}
+function connect() {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield client.connect();
+    });
+}
+function readfile(filename) {
+    let data = fs_1.default.readFileSync(filename, "utf8");
+    return data;
+}
+function setToDB(key, data) {
+    client.setEx(key, 10, data);
+}
+function getDataFromDB() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let data = yield client.get('text');
+        console.log("data from db: ", data);
+    });
+}
+function deleteDataFromDB(key) {
+    console.log("deleting: ", key);
+    client.del(key);
+}
+function exportRedisData() {
+    let exportFile = "";
+    let content = "";
+    fs_1.default.writeFile(exportFile, content, err => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+    });
+}
+app.listen(process.env.PORT || 3001);
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,53 @@
-import fs from "fs";
-console.log("hello world");
+import fs, { readFile } from "fs";
+import * as redis from "redis";
+import express from "express";
 
-const fileName = "./src/sample.txt";
-fs.readFile(fileName, "utf8", (err, data) =>{
-    if(err){
-        console.error(err);
-        return;
-    }
-    console.log("data: ",data);
-});
+const client = redis.createClient();
+const app = express();
+
+client.on('error', (err) => console.log('Redis Client Error', err));
+
+
+connect();
+const fileName: string = "./src/sample.txt";
+const key: string = "text";
+
+main();
+function main(){
+    setToDB(key, readfile(fileName));
+    setTimeout(() => {
+        getDataFromDB();
+    },5000)
+    setTimeout(() => {
+        deleteDataFromDB(key);
+    },7000)
+    setTimeout(() => {
+        getDataFromDB();
+    },8000)
+}
+
+async function connect(){
+    await client.connect();
+}
+
+function readfile(filename: string){
+    let data = fs.readFileSync(filename, "utf8");
+    return data;
+}
+
+function setToDB(key: string,data: string){
+    client.setEx(key, 10, data);
+}
+
+async function getDataFromDB(){
+    let data = await client.get('text');
+    console.log("data from db: ",data);
+}
+
+function deleteDataFromDB(key: string){
+    console.log("deleting: ",key);
+    client.del(key);
+}
 
 function exportRedisData(){
     let exportFile: string = "";
@@ -21,3 +60,5 @@ function exportRedisData(){
     })
 
 }
+
+app.listen(process.env.PORT || 3001);
